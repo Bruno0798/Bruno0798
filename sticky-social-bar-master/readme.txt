ac4657b84666b417c90d9b4301470f8a1cb83e88
BrunoMiguel <72737592+Bruno0798@users.noreply.github.com>
2023-09-12 11:35:14 +0100

    Add files via upload

---
+++ sticky-social-bar-master/readme.txt
@@ -0,0 +1,68 @@
+=== Plugin Name ===
+Contributors: eftakhairul
+Donate link:
+Tags: social, sticky, bar, side bar, Sticky Social Bar, sticky-social-bar
+Requires at least: 3.0.1
+Tested up to: 3.9.2
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Sticky social bar is a simple container of social media icons with links, glued to the right side of the browser window
+
+== Description ==
+
+Sticky social bar is a simple container of social media icons, glued to the right side of the browser window.
+Each element in the sticky social bar is animated using css3 transitions and transform rules
+
+
+= Features =
+  * Easy and simple.
+  * Not required any high level animation, only by css3
+  * Easy setting and configuration
+
+= Author =
+Brought to you by [Md. Eftakhairul Islam](http://eftakhairul.com)
+
+= Contribute =
+This may have bugs and lack of many features. If you want to contribute on this project, you are more than welcome. Please fork the repository from [Github](https://github.com/eftakhairul/Sticky-Social-Bar).
+
+Please [donate](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=eftakhairul%40gmail%2ecom&lc=CA&item_name=Eftakhairul%20world&item_number=web_product&currency_code=CAD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted) me to enhance this project.
+
+== Installation ==
+
+Just flow the following easy steps..
+
+1. Click the 'Sticky Social Bar' button at admin menu
+2. Put your social links and press Save Changes Button
+
+== Frequently Asked Questions ==
+
+= Q. What type of link will I have to put? =
+A. Any social link you can put.
+
+= Q. Would please show me an example? =
+
+A. If your facebook link is like: fb.com/your_name, then put it next to Facebook and press 'Save Changes' button
+
+
+== Screenshots ==
+
+1. Sticky Social Bar button at admin menu.
+2. Setting Page with save button
+3. Real UI
+
+== Changelog ==
+
+= 1.0 =
+Initial version released
+
+= 2.0 =
+CSS bugs are fixed
+
+== Upgrade Notice ==
+Nothing here
+
+
+
+
---
+++ sticky-social-bar-master/sticky-social-bar.php
@@ -0,0 +1,136 @@
+<?php
+/*
+Plugin Name: Sticky Social Bar
+Plugin URI: http://wordpress.org/plugins/sticky-social-bar/
+Description: Sticky social bar is a simple container of social media icons, glued to the right side of the browser window. 
+Version: 2.0
+Author: Eftakhairul Islam 
+Author URI: http://eftakhairul.com
+Wordpress version supported: 3.0 and above
+License: GPL2
+*/
+
+include("installation-sticky-social-bar.php");
+
+register_activation_hook (__FILE__,'stickysocialbar_install');
+register_deactivation_hook (__FILE__,'stickysocialbar_uninstall');
+
+
+
+//Admin menu
+if ( is_admin() ) {
+    add_action('admin_menu', 'stickysocialbar_admin_create_menu');
+}
+
+
+/**
+ *  Create admin menu
+ */
+function stickysocialbar_admin_create_menu()
+{
+    add_menu_page('Sticky Social Bar', 'Sticky Social Bar', 'administrator', 'sticky-social-bar-setting', 'sticky_social_bar_setting_form',plugins_url('/sticky-social-bar/images/icon_pref_settings.gif',1));
+}
+
+/**
+ *  Load the setting from
+ */
+function sticky_social_bar_setting_form()
+{
+    include("sticky-social-bar-setting-form.php");
+}
+
+//Main function Sticky Social Bar
+add_action('wp_head', 'sticky_social_bar_css');
+
+//CSS
+function sticky_social_bar_css() {
+?>
+<style type="text/css">
+    .sticky-container {
+		/*background-color: #333;*/
+		padding: 0px;
+		margin: 0px;
+		position: fixed;
+		right: -129px;
+		top:130px;
+		width: 200px;
+		z-index: 10000;
+	}
+
+	.sticky li {
+		list-style-type: none;
+		background-color: #333;
+		color: #efefef;
+		height: 43px;
+		padding: 0px;
+		margin: 0px 0px 1px 0px;
+		-webkit-transition:all 0.25s ease-in-out;
+		-moz-transition:all 0.25s ease-in-out;
+		-o-transition:all 0.25s ease-in-out;
+		transition:all 0.25s ease-in-out;
+		cursor: pointer;
+		filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale");
+                filter: gray;
+                -webkit-filter: grayscale(100%);
+
+	}
+
+	.sticky li:hover {
+		margin-left: -115px;
+		/*-webkit-transform: translateX(-115px);
+		-moz-transform: translateX(-115px);
+		-o-transform: translateX(-115px);
+		-ms-transform: translateX(-115px);
+		transform:translateX(-115px);*/
+		/*background-color: #8e44ad;*/
+		filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'1 0 0 0 0, 0 1 0 0 0, 0 0 1 0 0, 0 0 0 1 0\'/></filter></svg>#grayscale");
+                -webkit-filter: grayscale(0%);
+	}
+
+	.sticky li a img {
+		float: left;
+		margin: 5px 5px;
+		margin-right: 10px;
+
+	}
+
+	.sticky li a p {
+		padding: 0px;
+		margin: 0px;
+		text-transform: uppercase;
+		line-height: 43px;
+        text-decoration: none !important;
+        color: #ECF0F1;
+        font-family: "Lato";
+    }
+</style>
+<?php
+}
+
+//HTML
+add_action( 'get_footer', 'sticky_social_bar_html' );
+
+function sticky_social_bar_html() {
+    global $wpdb;
+    $stickySocialBarTable  = $wpdb->prefix . "sticky_social_bar";
+    $result = $wpdb->get_results("SELECT * FROM {$stickySocialBarTable} WHERE id = 1");
+    $links   = get_object_vars($result[0]);
+    unset($links['id']);
+    unset($links['update_date']);
+?>
+<div class="sticky-container">
+    <ul class="sticky">
+        <?php foreach($links as $name => $link) : ?>
+            <?php if(!empty($link)): ?>
+            <li>
+                <a target="_blank" href="<?php echo $link ?>">
+                    <img width="32" height="32" alt="" src="<?php echo plugins_url('/sticky-social-bar/images/'. $name .'.png', 1) ?>" />
+                    <p><?php echo ucfirst($name) ?></p>
+                </a>
+            </li>
+            <?php endif ?>
+        <?php endforeach ?>
+    </ul>
+</div>
+<?php
+}
